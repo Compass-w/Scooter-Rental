9589857f7627b41feb59a9bcf573b0841423eb5b
Compass_w <compass_w@Compass-wdeMacBook-Pro.local>
2026-04-15 14:45:33 +0800

    chore: add frontend dist build output to main

---
+++ frontend/dist/build/h5/assets/issue.Cef2VtgJ.js
@@ -0,0 +1,1 @@
+import{r as s}from"./request.CgzZ_Qtf.js";const t=t=>s.post("/issues",t),a=(t={})=>s.get("/admin/issues",t),e=(t,a)=>s.put(`/admin/issues/${t}/status`,{status:a});export{a as g,t as r,e as u};
